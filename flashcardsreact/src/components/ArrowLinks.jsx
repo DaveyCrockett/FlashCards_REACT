60e529bb50747dadacb6e5a41d5aa44b43ee71b0
DAVID paredes <augustspies22@gmail.com>
2021-06-04 08:37:50 -0700

    Added axios calls which dont work yet and extra components.

---
+++ flashcardsreact/src/components/ArrowLinks.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ArrowLinks extends Component {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            
+        }
+     }
+
+
+    render() { 
+        return ( 
+            
+         );
+    }
+}
+ 
+export default ArrowLinks;
--- flashcardsreact/src/components/app.jsx
+++ flashcardsreact/src/components/app.jsx
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
+import GetCollection from './GetCollection';
 
 class App extends Component {
-    state = { }
+    constructor(props){
+        super(props)
+        this.state = {
+           flashcards: [],
+        }
+    }
+
+    async get_collection(collection_id) {
+        try{
+            let response = await axios.get(`http://127.0.0.1:8000/flashcardsapp/${collection_id}`);
+            this.setState({
+                flashcards: response.data
+            });
+        } catch (er){
+            console.log('ERROR in get_collection', er)
+        }
+
+    }
+
+    async get_flashcard(collection_id, flash_card_id) {
+        try{
+            let response = await axios.get(`http://127.0.0.1:8000/flashcardsapp/${collection_id}/${flash_card_id}`)
+            this.setState({
+                flashcards: response.data
+            });
+        } catch (er){
+            console.log('ERROR in get_flashcard', er)
+        }
+    }
+
+    async post_flashcard() {
+        const flashCards = {flashcards: this.state.flashcards};
+        await axios.post(`http://127.0.0.1:8000/flashcardsapp/${}`, { flashCards })
+        .then(response => this.setState({
+            flashcards: [...this.state.flashcards, response.data]
+        }))
+        
+    }
+
+    async post_flashcard(collection_id, flash_card_id) {
+        const flashCards = {flashcards: this.state.flashcards};
+        await axios.put(`http://127.0.0.1:8000/flashcardsapp/${collection_id}/${flash_card_id}`, { flashCards })
+        .then(response => this.setState({
+            flashcards: [...this.state.flashcards, response.data]
+        }))
+        
+    }
+
     render() {
         return (
-            <h1>Our React App Using a Component</h1>
+            <div>
+                <table>
+                    <tbody>
+                        <GetCollection />
+                    </tbody>
+                </table>
+            </div>
         );
-    }
+    };
 }
 
 export default App;
