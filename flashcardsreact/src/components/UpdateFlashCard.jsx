425237a721078e688e1c54862da099be7d25ed9b
DAVID paredes <augustspies22@gmail.com>
2021-06-04 07:37:58 -0700

    set up complete and added skeletons of components.

---
+++ flashcardsreact/src/components/UpdateFlashCard.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class UpdateFlashCard extends Component {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            
+        }
+     }
+
+
+    render() { 
+        return ( 
+            
+         );
+    }
+}
+ 
+export default UpdateFlashCard;
